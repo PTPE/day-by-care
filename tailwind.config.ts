f4951d6f3f0b412682445867a067cb25acefebdb
YuJieLeee <yujie@lightup.tw>
2024-08-29 17:42:01 +0800

    feat: color setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+
+        'button-primary-text': 'var(--color-button-primary-text)',
+      },
+      backgroundColor: {
+        primary: 'var(--color-bg)',
+        'button-primary': 'var(--color-button-primary-bg)',
+        'button-primary-hover': 'var(--color-button-primary-bg-hover)',
+        'button-secondary': 'var(--color-button-secondary-bg)',
+        'button-secondary-hover': 'var(--color-button-secondary-bg-hover)',
+      },
+      textColor: {
+        primary: 'var(--color-text)',
+        'button-primary': 'var(--color-button-text)',
       },
     },
   },
