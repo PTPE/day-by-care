13ff680408143ef4ea017eaa95112c71dfb14c03
YuJieLeee <yujie@lightup.tw>
2024-10-25 23:32:03 +0800

    fix: header ui

--- src/ui/header/header.tsx
+++ src/ui/header/header.tsx
@@ -10,8 +10,8 @@ type Props = {
 
 export default function Header({ middleChildren, endChildren }: Props) {
   return (
-    <div className="flex flex-col gap-1 bg-secondaryBg shadow-[0_5px_15px_0px_rgba(17,17,26,0.1)] p-2">
-      <div className="flex items-center justify-between max-w-[1190px] w-full flex-grow mx-auto ">
+    <div className="flex flex-col gap-1 bg-secondary shadow-[0_5px_15px_0px_rgba(17,17,26,0.1)] p-2">
+      <div className="flex items-center justify-between max-w-[1190px] w-full flex-grow mx-auto">
         <Link href="/">
           <Logo size="md" className="cursor-pointer" />
         </Link>
