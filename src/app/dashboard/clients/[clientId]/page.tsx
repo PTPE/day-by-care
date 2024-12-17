ed2b6842911d7cf321dc7c09618f086326a773d5
YuJieLeee <yujie@lightup.tw>
2024-12-18 00:23:39 +0800

    feat: view client profile

--- src/app/dashboard/clients/[clientId]/page.tsx
+++ src/app/dashboard/clients/[clientId]/page.tsx
@@ -1,96 +1,87 @@
 'use client';
 
-import { useState } from 'react';
+import { useParams } from 'next/navigation';
+import { format } from 'date-fns';
 
-import ClientIconSelector from '@/features/client/components/client-icon-selector/client-icon-selector';
-import ServiceItemSelector from '@/features/client/components/service-items-selector';
-import Input from '@/ui/input';
-import Label from '@/ui/label';
+import useGetClient from '@/features/client/hooks/useGetClient';
+import Badge from '@/ui/badge';
+import Button from '@/ui/button';
 
 export default function ClientPage() {
-  const [selectedClientIcon, setSelectedClientIcon] = useState('');
-  const [selectedServiceItems, setSelectedServiceItems] = useState<string[]>(
-    []
-  );
+  const { clientId } = useParams<{ clientId: string }>();
+  const { data: client } = useGetClient(clientId);
 
-  const handleSelectedServiceItems = (item: string) => {
-    setSelectedServiceItems((prev) => {
-      if (prev.includes(item)) {
-        return prev.filter((i) => i !== item);
-      }
-      return [...prev, item];
-    });
-  };
+  if (!client) return null;
 
   return (
-    <div className="flex flex-col items-center justify-center gap-5 w-1/2 m-auto">
-      <div className="grid grid-cols-2 gap-5 w-full">
-        <div className="col-span-2">
-          <ClientIconSelector
-            onSelectIcon={(icon) => {
-              setSelectedClientIcon(icon);
-            }}
-            selectedIcon={selectedClientIcon}
-          />
+    <div className="flex">
+      <div className="relative flex flex-col m-auto gap-5">
+        <Button className="absolute top-0 right-0">編輯</Button>
+        <div className="w-20 h-20 p-3 shadow-[0_5px_15px_0px_rgba(17,17,26,0.1)] rounded-full bg-secondary flex items-center justify-center cursor-pointer self-center">
+          <span className={`${client.clientIcon} text-7xl`} />
+        </div>
+        <p className="self-center">{client.clientName}</p>
+
+        <div className="space-x-5 self-center">
+          <Button variant="outline" className="border-2">
+            <span className="icon-[material-symbols--call]" />
+            <span>撥打電話</span>
+          </Button>
+          <Button variant="outline" className="border-2">
+            <span className="icon-[material-symbols--location-on]" />
+            <span>地點</span>
+          </Button>
         </div>
 
-        <div className="grid items-center gap-1.5 w-full">
-          <Label htmlFor="name">姓名</Label>
-          <Input type="name" id="name" placeholder="姓名" />
-        </div>
+        <div className="flex flex-col gap-5">
+          <p className="font-extrabold">個人資訊</p>
 
-        <div className="grid items-center gap-1.5">
-          <Label htmlFor="birthday">生日</Label>
-          <Input type="birthday" id="birthday" placeholder="生日" />
-        </div>
+          <div className="flex gap-2 text-sm">
+            <p className=" w-18">地址：</p>
+            <p>{client.address}</p>
+          </div>
 
-        <div className="grid items-center gap-1.5 w-full col-span-2">
-          <Label htmlFor="address">住址</Label>
-          <Input type="address" id="address" placeholder="住址" />
-        </div>
+          <div className="flex gap-2 text-sm">
+            <p className=" w-18">生日：</p>
+            <p>{format(new Date(client.birthday), 'yyyy/MM/dd')}</p>
+          </div>
 
-        <div className="col-span-2 w-full">
-          <ServiceItemSelector
-            selectedServiceItems={selectedServiceItems}
-            onHandleSelectedServiceItems={handleSelectedServiceItems}
-          />
-        </div>
+          <p className="font-extrabold">緊急聯絡人</p>
 
-        <div>
-          <Label htmlFor="emergencyContact">緊急聯絡人</Label>
-          <Input
-            type="emergencyContact"
-            id="emergencyContact"
-            placeholder="姓名"
-          />
-        </div>
+          <div className="flex gap-2 text-sm">
+            <p className="w-18">姓名：</p>
+            <p>{client.emergencyContact}</p>
+          </div>
 
-        <div>
-          <Label htmlFor="emergencyContactPhone">緊急聯絡人電話</Label>
-          <Input
-            type="emergencyContactPhone"
-            id="emergencyContactPhone"
-            placeholder="電話"
-          />
-        </div>
+          <div className="flex gap-2 text-sm">
+            <p className="w-18">電話：</p>
+            <p>{client.emergencyContactPhone}</p>
+          </div>
 
-        <div>
-          <Label htmlFor="supervisorName">督導姓名</Label>
-          <Input type="supervisorName" id="supervisorName" placeholder="姓名" />
-        </div>
+          <p className="font-extrabold">服務資訊</p>
 
-        <div>
-          <Label htmlFor="supervisorPhone">督導電話</Label>
-          <Input
-            type="supervisorPhone"
-            id="supervisorPhone"
-            placeholder="電話"
-          />
-        </div>
+          <div className="flex gap-2 text-sm items-center">
+            <p className="w-18">服務項目：</p>
+            {client.serviceItems.map((item) => (
+              <Badge key={item} variant="outline">
+                {item}
+              </Badge>
+            ))}
+          </div>
 
-        <div>
-          <Label htmlFor="officePhone">辦公室電話</Label>
-          <Input type="officePhone" id="officePhone" placeholder="電話" />
+          <div className="flex gap-2 text-sm items-center">
+            <p className="w-18">督導姓名：</p>
+            <p>{client.supervisorName}</p>
+          </div>
+          <div className="flex gap-2 text-sm items-center">
+            <p className="w-18">督導電話：</p>
+            <p>{client.supervisorPhone}</p>
+          </div>
+
+          <div className="flex gap-2 text-sm items-center">
+            <p className="w-18">辦公室電話：</p>
+            <p>{client.officePhone}</p>
+          </div>
         </div>
       </div>
     </div>
--- src/ui/badge/badge.tsx
+++ src/ui/badge/badge.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-md font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-full border px-2.5 py-0.5 text-md transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
@@ -15,7 +15,7 @@ const badgeVariants = cva(
           'border-transparent bg-secondary-button text-secondary-foreground hover:bg-secondary-button/80',
         destructive:
           'border-transparent bg-destructive-button text-destructive-foreground hover:bg-destructive-button/80',
-        outline: 'text-foreground',
+        outline: 'text-foreground border border-foreground',
       },
     },
     defaultVariants: {
