be0c50ffaf3d966d0012ef67b9a35e7e99d00263
YuJieLeee <yujie@lightup.tw>
2025-02-18 23:01:59 +0800

    fix: update react query

--- src/providers/tanstakeProvider/TanstakeProvider.tsx
+++ src/providers/tanstakeProvider/TanstakeProvider.tsx
@@ -1,13 +1,34 @@
 'use client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 
-export default function TanstakeProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const queryClient = new QueryClient();
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  }
+  if (!browserQueryClient) browserQueryClient = makeQueryClient();
+  return browserQueryClient;
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = getQueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
