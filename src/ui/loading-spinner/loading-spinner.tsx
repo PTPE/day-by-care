49fe8db07dbc44944acd39e16f8ae7592b230669
YuJieLeee <yujie@lightup.tw>
2025-06-21 14:43:10 +0800

    fix: loading spinner style

--- src/ui/loading-spinner/loading-spinner.tsx
+++ src/ui/loading-spinner/loading-spinner.tsx
@@ -1,6 +1,6 @@
 export default function LoadingSpinner() {
   return (
-    <div className="fixed backdrop-blur-[1px] flex items-center justify-center z-50 h-screen w-screen top-0 left-0">
+    <div className="fixed backdrop-blur-[1px] flex items-center justify-center z-[9999] h-full w-full top-0 left-0">
       <div className="animate-spin rounded-full h-16 w-16 border-4 border-solid border-transparent border-t-primary z-[9999]"></div>
     </div>
   );
